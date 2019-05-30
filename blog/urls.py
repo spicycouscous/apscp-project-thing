726aa80a66dcd1ad6b470cdaee6c5ced4a298ee3
spicycouscous <nope>
2019-05-30 14:16:33 -0700

    init and final

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # TODO: change to modal and remove url path
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+]
---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Documents\cspproj1\djangoweb\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
